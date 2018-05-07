4edcff851f6c4c85138ab769c38b373eabd2b0a8
Meenakshi Kumari <meenakshi697@gmail.com>
2018-05-07 18:50:02 -0500

    Problems on Matrices

---
+++ Matrices/MakeRowAndColumnZeroOptimized.java
@@ -0,0 +1,85 @@
+package Matrices;
+
+public class MakeRowAndColumnZeroOptimized {
+
+	public static void modifyMatrix(int mat[][]){
+        
+        // variables to check if there are any 1 
+        // in first row and column
+        boolean row_flag = false;
+        boolean col_flag = false;
+        int i,j;
+                 
+        // updating the first row and col if 1
+        // is encountered
+        for (i = 0; i < mat.length; i++ ){
+                for (j = 0; j < mat[0].length; j++){
+                        if (i == 0 && mat[i][j] == 1)
+                            row_flag = true;
+                         
+                        if (j == 0 && mat[i][j] == 1)
+                            col_flag = true;
+                         
+                        if (mat[i][j] == 1){
+                             
+                            mat[0][j] = 1;
+                            mat[i][0] = 1;
+                        }
+                         
+                    }
+                }
+                 
+        // Modify the input matrix mat[] using the 
+        // first row and first column of Matrix mat
+        for (i = 1; i < mat.length; i ++){
+                for (j = 1; j < mat[0].length; j ++){
+                         
+                    if (mat[0][j] == 1 || mat[i][0] == 1){
+                            mat[i][j] = 1;
+                        }
+                    }
+                }
+                 
+        // modify first row if there was any 1
+        if (row_flag == true){
+            for (i = 0; i < mat[0].length; i++){
+                        mat[0][i] = 1;
+                    }
+                }
+                 
+        // modify first col if there was any 1
+        if (col_flag == true){
+            for (i = 0; i < mat.length; i ++){
+                        mat[i][0] = 1;
+            }
+        }
+    }
+             
+    /* A utility function to print a 2D matrix */
+    public static void printMatrix(int mat[][]){
+        for (int i = 0; i < mat.length; i ++){
+            for (int j = 0; j < mat[0].length; j++){
+                System.out.print( mat[i][j] );
+            }
+                System.out.println("");
+        }
+    }
+             
+    // Driver function to test the above function
+    public static void main(String args[] ){
+                 
+        int mat[][] = {{1, 0, 0, 1},
+                {0, 0, 1, 0},
+                {0, 0, 0, 0}};
+                 
+        System.out.println("Input Matrix :");
+        printMatrix(mat);
+             
+        modifyMatrix(mat);
+             
+        System.out.println("Matrix After Modification :");
+        printMatrix(mat);
+             
+    }
+ 
+}
